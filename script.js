35a7fbe757c4f28b86efd64583b7fa6ff87479a6
Pavel Moskov <pavel.moskov@gmail.com>
2023-06-10 15:27:41 +0300

    sprint 4 html css js

---
+++ script.js
@@ -0,0 +1,33 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popUp = document.querySelector('.popup__overlay__popup_opened');
+let formElement = popUp.querySelector('.popup__container');
+let closeButton = formElement.querySelector('.popup__close-button');
+
+function openPopUp() {
+  popUp.setAttribute('style', 'display: flex');
+}
+
+editButton.addEventListener('click', openPopUp);
+
+function closePopUp() {
+  popUp.setAttribute('style', 'display: none');
+}
+
+closeButton.addEventListener('click', closePopUp);
+
+
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
+
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+    let userName = nameInput.value;
+    let userJob = jobInput.value;
+    let profileName = document.querySelector('.profile__name');
+    let profileJob = document.querySelector('.profile__job');
+    profileName.textContent = userName;
+    profileJob.textContent = userJob;
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
