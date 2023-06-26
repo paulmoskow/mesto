c9f52a3ab9cd5587fc06c87c52420d74d615eb92
Pavel Moskov <pavel.moskov@gmail.com>
2023-06-26 14:14:37 +0300

    feat; add initial cards with js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,50 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const userElements = document.querySelector('.elements');
+const userTemplate = document.querySelector('#element').content;
+
+const cardsElements = []
+for (let i = 0; i < initialCards.length; i++) {
+  const userElement = userTemplate.querySelector('.element').cloneNode(true);
+  userElement.querySelector('.element__image').src = initialCards[i].link;
+  userElement.querySelector('.element__image').alt = initialCards[i].name;
+  userElement.querySelector('.element__text').textContent = initialCards[i].name;
+  cardsElements[i] = userElement;
+}
+
+for (let i = 0; i < cardsElements.length; i++) {
+  userElements.prepend(cardsElements[i]);
+}
+
+
+
+
+
 let editButton = document.querySelector('.profile__edit-button');
 let popUp = document.querySelector('.popup');
 let formElement = popUp.querySelector('.popup__form');
